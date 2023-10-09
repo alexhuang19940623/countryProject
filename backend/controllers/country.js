eb08f7d670a7dd8ceb891d66b5f3c21c82648407
alexhuang19940623 <tianyu.huang@ucdconnect.ie>
2023-10-09 20:23:37 +0100

    final

--- backend/controllers/country.js
+++ backend/controllers/country.js
@@ -1,27 +1,34 @@
-
+const fetch = require('node-fetch'); // import node-fetch
 
 exports.getCountry = async (req, res) => {
-  const {countryText}=req.body
-  await fetch('https://restcountries.com/v3.1/name/'+countryText, {
-    method: 'GET',
-    headers: {
-      'Content-type': 'application/json; charset=UTF-8',
-    },
+  const { countryText } = req.body;
+
+  if (!countryText) {
+    return res.status(400).json({ message: "Country name is required" });
   }
-  ).then((res) => res.json())
-  .then((data) => {
-    if(data.status==404){
-     return res.status(404).json({
-        "message":data.message
+
+  try {
+    const apiRes = await fetch('https://restcountries.com/v3.1/name/' + countryText, {
+      method: 'GET',
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    });
+
+    const data = await apiRes.json();
+
+    if(apiRes.status !== 200) {
+      return res.status(apiRes.status).json({
+        message: data.message || "Error fetching data from API"
       });
     }
-    return res.status(200).json({
-      "data":data
-    });
-  })
-  .catch(err => {
-    return res.status(400).json({"message":"cause: Error: Client network socket disconnected before secure TLS connection was established"});
-  });
- 
-};
 
+    return res.status(200).json({
+      data: data
+    });
+
+  } catch (err) {
+    console.error(err); // Log the error for debugging
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
--- backend/routes/country.js
+++ backend/routes/country.js
@@ -4,9 +4,7 @@ const router = express.Router();
 // controller
 const { getCountry } = require("../controllers/country");
 
-
 // routes
 router.post("/country", getCountry);
-
 
 module.exports = router;
