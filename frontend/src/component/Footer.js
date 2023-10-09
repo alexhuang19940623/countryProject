f19337b96dc54b9ba8f802e2b7373614c3094096
alexhuang19940623 <tianyu.huang@ucdconnect.ie>
2023-10-09 19:28:41 +0100

    init

---
+++ frontend/src/component/Footer.js
@@ -0,0 +1,14 @@
+import React, { useState,useEffect, useMemo } from "react";
+import { Form, Input, Button,Checkbox,Alert,Menu,notification,List, Avatar } from 'antd';
+import '../style.css';
+
+function Footer() {
+
+    return(
+      <div className="footer_box">
+        @2023 /10 /9
+      </div>
+    )
+}
+
+export default Footer;
