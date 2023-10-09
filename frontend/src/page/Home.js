67f57787193e0af1e9f306b930ca1aed61f9e0dd
alexhuang19940623 <tianyu.huang@ucdconnect.ie>
2023-10-09 20:00:53 +0100

    init

--- frontend/src/page/Home.js
+++ frontend/src/page/Home.js
@@ -20,7 +20,7 @@ function Home() {
   let handleSubmit = e => {
     e.preventDefault();
     if(text!=""){
-      axios.post('192.168.147.129:8000/api/country', {
+      axios.post('127.0.0.1:8000/api/country', {
         "countryText":text
       }).then(response => {
          openNotificationWithIcon('success','Search Tips',"Search Successfully!");
